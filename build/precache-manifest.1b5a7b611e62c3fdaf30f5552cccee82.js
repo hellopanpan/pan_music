61caebbb571e7c5c7eded421e7dcdd283fd14f6f
panmin <minp@jumei.com>
2020-10-16 18:24:37 +0800

    build

---
+++ build/precache-manifest.1b5a7b611e62c3fdaf30f5552cccee82.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d53e49304cf89c0348ad648d6533633",
+    "url": "http://xpanpan.com/index.html"
+  },
+  {
+    "revision": "b886ef6fad0fd85e399d",
+    "url": "http://xpanpan.com/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "4f51c91e35e785bb1711",
+    "url": "http://xpanpan.com/static/css/main.abc6fb63.chunk.css"
+  },
+  {
+    "revision": "b886ef6fad0fd85e399d",
+    "url": "http://xpanpan.com/static/js/2.912dbd3a.chunk.js"
+  },
+  {
+    "revision": "72f445d4908265d5a816299099ad1499",
+    "url": "http://xpanpan.com/static/js/2.912dbd3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f51c91e35e785bb1711",
+    "url": "http://xpanpan.com/static/js/main.b0ed5c93.chunk.js"
+  },
+  {
+    "revision": "6bb9a304159d8dc0ddc7",
+    "url": "http://xpanpan.com/static/js/runtime-main.5dc0c8e5.js"
+  },
+  {
+    "revision": "0ff7910e55b51afc729545a18a7365b9",
+    "url": "http://xpanpan.com/static/media/iconfont.0ff7910e.ttf"
+  },
+  {
+    "revision": "605a20c22d03c5eedd16cb97189b0d8b",
+    "url": "http://xpanpan.com/static/media/iconfont.605a20c2.svg"
+  },
+  {
+    "revision": "bba8a4918da82c74d60ebef37867cf94",
+    "url": "http://xpanpan.com/static/media/iconfont.bba8a491.woff"
+  },
+  {
+    "revision": "da15fc11f9424e6230750c4b256c503f",
+    "url": "http://xpanpan.com/static/media/iconfont.da15fc11.eot"
+  }
+]);
