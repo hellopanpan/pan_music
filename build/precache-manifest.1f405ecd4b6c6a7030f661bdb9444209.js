70fb54f0727189c079ca922a227dbfe1ab9a9380
panmin <minp@jumei.com>
2021-01-08 11:10:52 +0800

    异步路由

---
+++ build/precache-manifest.1f405ecd4b6c6a7030f661bdb9444209.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146bfb642ab5c234186371973557ff5f",
+    "url": "/pan_music/build/index.html"
+  },
+  {
+    "revision": "6eddfbfe05a45b10fd3c",
+    "url": "/pan_music/build/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "73f703da54b6173c1442",
+    "url": "/pan_music/build/static/css/main.779cd7e3.chunk.css"
+  },
+  {
+    "revision": "6eddfbfe05a45b10fd3c",
+    "url": "/pan_music/build/static/js/2.6312ef9d.chunk.js"
+  },
+  {
+    "revision": "3dd94b94385dac114e909f86af7aa28e",
+    "url": "/pan_music/build/static/js/2.6312ef9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a0fb0051a6ebbcb8767",
+    "url": "/pan_music/build/static/js/3.348d4aed.chunk.js"
+  },
+  {
+    "revision": "6a8b1fe75cfa9d425542",
+    "url": "/pan_music/build/static/js/4.812f096f.chunk.js"
+  },
+  {
+    "revision": "80057b1828bc39e98518",
+    "url": "/pan_music/build/static/js/5.df34dc2f.chunk.js"
+  },
+  {
+    "revision": "c2679da4bc119f896a93",
+    "url": "/pan_music/build/static/js/6.4c02f6e2.chunk.js"
+  },
+  {
+    "revision": "4a3c94716d76ab26a5cc",
+    "url": "/pan_music/build/static/js/7.c2f1a28a.chunk.js"
+  },
+  {
+    "revision": "a97d54a50ee34c091b82",
+    "url": "/pan_music/build/static/js/8.28a32721.chunk.js"
+  },
+  {
+    "revision": "73f703da54b6173c1442",
+    "url": "/pan_music/build/static/js/main.d8b7d600.chunk.js"
+  },
+  {
+    "revision": "2fbbdcd9597e1f040de5",
+    "url": "/pan_music/build/static/js/runtime-main.f5259753.js"
+  },
+  {
+    "revision": "0ff7910e55b51afc729545a18a7365b9",
+    "url": "/pan_music/build/static/media/iconfont.0ff7910e.ttf"
+  },
+  {
+    "revision": "605a20c22d03c5eedd16cb97189b0d8b",
+    "url": "/pan_music/build/static/media/iconfont.605a20c2.svg"
+  },
+  {
+    "revision": "bba8a4918da82c74d60ebef37867cf94",
+    "url": "/pan_music/build/static/media/iconfont.bba8a491.woff"
+  },
+  {
+    "revision": "da15fc11f9424e6230750c4b256c503f",
+    "url": "/pan_music/build/static/media/iconfont.da15fc11.eot"
+  }
+]);
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,40 @@
-import Header from '@/views/header/index'
-import Detail from '@/views/detail/index'
-import Search from '@/views/search/index'
-import SingerDetail from '@/views/singerDetail/index'
+import React, { lazy, Suspense } from "react";
+import { Redirect } from "react-router-dom";
+// import Detail from '@/views/detail/index'
+// import Search from '@/views/search/index'
+// import SingerDetail from '@/views/singerDetail/index'
+const SuspenseComponent = Component => props => {
+  return (
+    <Suspense fallback={null}>
+      <Component {...props}></Component>
+    </Suspense>
+  )
+}
+
+const Header = lazy(() => import("@/views/header/index"));
+const Detail = lazy(() => import("@/views/detail/index"));
+const Search = lazy(() => import("@/views/search/index"));
+const SingerDetail = lazy(() => import("@/views/singerDetail/index"));
+// import Header from '@/views/header/index'
+
 const routes = [
   {
     path:'/detail',
-    component: Detail,
+    component:  SuspenseComponent(Detail),
     exact:true
   },
   {
     path:'/search',
-    component: Search,
+    component: SuspenseComponent(Search),
     exact:true
   },
   {
     path:'/',
-    component: Header,
+    component: SuspenseComponent(Header),
     routes: [
       {
         path: "/singerDetail/:id",
-        component: SingerDetail,
+        component: SuspenseComponent(SingerDetail),
       }
     ]
   },
